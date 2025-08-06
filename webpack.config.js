1b89155cd5e03fb6691621f11a27fd0e228266c2
JuiceChamp <freckmannchase@gmail.com>
2025-08-06 16:01:31 -0400

    dealing with issues ugh

--- webpack.config.js
+++ webpack.config.js
@@ -15,16 +15,24 @@ module.exports = {
         template: "./src/home.html",
     }),
   ],
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/home.html"],
+  },
   module: {
     rules: [
         {
             test: /\.css$/i,
             use: ["style-loader", "css-loader"],
         },
+        {
+            test: /\.html$/i,
+            loader: "html-loader"
+        },
+        {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: "asset/resource",
+        },
     ],
-  },
-  {
-    test: /\.html$/i,
-    loader: "html-loader",
   },
 };
